ab1df30e40d4fd9e0b0159ee2115714ecc877c17
wynnyr <33757411+wynnyr@users.noreply.github.com>
2018-04-22 18:27:03 +0700

    Mass QR Code Generator

--- client/campaign/campaign_item.js
+++ client/campaign/campaign_item.js
@@ -3,3 +3,30 @@ Template.campaignItem.helpers({
      return this.userId === Meteor.userId();
   },
 });
+
+Template.campaignItem.events({
+  'click .csvExport': function(e) {
+    e.preventDefault();
+    console.log("csvExport")
+
+  },
+
+  'click .bulkAdd': function (e) {
+    e.preventDefault();
+    var count = prompt("Mass QR Code Generator","3");
+    if (count == null || count == "") {
+      return;
+    }
+
+    Meteor.call('qrcodeBulk', this._id, count,function(error, result){
+      if (error){
+        return throwError(error.reason);
+      }
+
+      Router.go('campaignPage', {_id: result.campaignId}); 
+
+    });
+  }
+
+
+});
